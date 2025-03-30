2ef66d42b621813a2fa2cd17292547eb9b02ef9b
danny <xdannyrobertsx@gmail.com>
2025-03-30 13:46:45 -0500

    add deno-green

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Title from "./components/Title.tsx";
+import OTPExample from "./components/OTPExample.tsx";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <Title title="bruh!" />
+      <div className="flex flex-col items-center justify-center h-screen gap-4 bg-gray-100">
+        <Title />
+        <OTPExample />
       </div>
     </>
   );
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,31 +1,11 @@
-import {
-  InputOTP,
-  InputOTPGroup,
-  InputOTPSeparator,
-  InputOTPSlot,
-} from "./ui/OTP.tsx";
-
 export default function Title({
-  title = "Hello World!",
+  title = "Deno is awesome!",
 }) {
   return (
     <>
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl hover:border-b-4 hover:border-[var(--deno-green)] transition-all">
         {title}
       </h1>
-      <InputOTP maxLength={6}>
-        <InputOTPGroup>
-          <InputOTPSlot index={0} />
-          <InputOTPSlot index={1} />
-          <InputOTPSlot index={2} />
-        </InputOTPGroup>
-        <InputOTPSeparator />
-        <InputOTPGroup>
-          <InputOTPSlot index={3} />
-          <InputOTPSlot index={4} />
-          <InputOTPSlot index={5} />
-        </InputOTPGroup>
-      </InputOTP>
     </>
   );
 }
